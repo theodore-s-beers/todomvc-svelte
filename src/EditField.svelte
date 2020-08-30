01a3a2d682ddf14112f65130f31a8ff4a0e5d480
Theodore Beers <theo.beers@fu-berlin.de>
2020-08-31 00:41:37 +0200

    Add initial files

---
+++ src/EditField.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let valueAttr;
+  export let keydownListener;
+  export let blurListener;
+
+  let editField: HTMLInputElement;
+
+  onMount(() => {
+    editField.focus();
+  });
+</script>
+
+<input
+  bind:this={editField}
+  value={valueAttr}
+  class="edit"
+  on:keydown={keydownListener}
+  on:blur={blurListener} />
